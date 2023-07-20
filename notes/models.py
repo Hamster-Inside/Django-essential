5d4247bb41913ad4974c1d0415f34a194cefbb5e
Arek <abudkowski@interia.pl>
2023-07-20 06:59:27 +0200

    added some notes logic

--- notes/models.py
+++ notes/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Notes(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
